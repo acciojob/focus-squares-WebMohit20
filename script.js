57aefb30c0a50fafcc5963b083faa33a574783e7
WebMohit20 <2003mohitkhemani@gmail.com>
2024-10-09 08:46:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const square = document.querySelectorAll(".square");
+console.log(square)
+square.forEach(sq=>{
+	sq.addEventListener("mouseover",()=>{
+		for(let i =0;i<square.length;i++){
+			if(sq.id == `square${i+1}`)
+				continue
+		square[i].style.backgroundColor = "#6F4E37";
+		}
+	})
+	sq.addEventListener("mouseout",()=>{
+		for(let i =0;i<square.length;i++){
+			if(sq.id == `square${i+1}`)
+				continue
+		square[i].style.backgroundColor = "#E6E6FA";
+		}
+	})
+})
+
+
+
+
+
+
+
